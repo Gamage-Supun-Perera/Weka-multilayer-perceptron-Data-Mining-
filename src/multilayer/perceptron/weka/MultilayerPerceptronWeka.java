4b9bf624c8e0170945dd5d7ae580a658e4585edf
supun perera <supun.perera50@gmail.com>
2017-03-05 14:28:33 +0530

    first commit

---
+++ src/multilayer/perceptron/weka/MultilayerPerceptronWeka.java
@@ -0,0 +1,15 @@
+
+package multilayer.perceptron.weka;
+
+/**
+ *
+ * @author supun perera
+ */
+public class MultilayerPerceptronWeka {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
